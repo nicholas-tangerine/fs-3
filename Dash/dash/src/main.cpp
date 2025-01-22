e4b54baedc799759045ea7070526f5c134f86852
Nova Mondal <rialmondal@gmail.com>
2025-01-22 13:41:00 -0800

    formatted dash src files

--- Dash/dash/src/main.cpp
+++ Dash/dash/src/main.cpp
@@ -3,12 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "mbed.h"
 #include "drivers/BT817/address_mapping.h"
 #include "drivers/BT817/dl_commands.h"
+#include "mbed.h"
 
-int main()
-{
+int main() {
     printf("Hello, Mbed!\n");
     return 0;
 }
