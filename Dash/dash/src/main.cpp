3fdc86310879899e23720753bff9e0c4c8319f79
Nova Mondal <rialmondal@gmail.com>
2025-01-28 17:15:06 -0800

    dash mbed program set up for real this time

---
+++ Dash/dash/src/main.cpp
@@ -0,0 +1,17 @@
+#include "DigitalOut.h"
+#include "ThisThread.h"
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+
+int main()
+{
+   DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(100ms);
+    }
+    return 0;
+}
+
